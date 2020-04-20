54ff7db9f6758d5868ea8ef0cda926ff31776916
Edvin Walleborn <edvin@kth.se>
2020-04-20 14:57:34 +0200

    code add first

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
-document.write("Successfully loaded main.js");
+
+document.write("hello world");
+
 
 class Node {
+    constructor(prerequisite, name, color){
+        this.prerequisite = prerequisite;
+        this.courseName = name;
+        this.courseColor = color;
+        this.children = [];
+    }
 
+    addChild(node){
+        this.children.push(node);
+    }
 }
+
+
+
+temp1 = new Node("ID1020", "Algodata", "Svart");
+temp2 = new Node("IK1203", "Matte", "Vit");
+temp3 = new Node("SF1610", "Fysik", "Orange");
