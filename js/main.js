5406c1ac61f1458dc06d7fd7b3c0e0b8af914e55
PatrikZhong <zhong.patrik@gmail.com>
2020-04-20 18:14:16 +0200

    Asynchronous fetch request, can use string to work with

--- js/main.js
+++ js/main.js
@@ -1,6 +1,23 @@
-fetch('https://api.kth.se/api/kopps/v2/course/DD1321/detailedinformation')
-.then(response => response.json())
-.then(data => {
-  console.log(data) // Prints result from response.json() in getRequest
-})
-.catch(error => console.error(error))
+
+
+var string = ' ';
+const request = async() => {
+    const json = await fetch('https://api.kth.se/api/kopps/v2/course/DD1321/detailedinformation').then(response => response.json());
+    console.log(json)
+
+}
+
+string = JSON.stringify(request());
+
+
+
+// var out = '';
+// console.log(out);
+// fetch('https://api.kth.se/api/kopps/v2/course/DD1321/detailedinformation')
+// .then(response => response.json())
+// .then(data => {
+// console.log(data);
+// })
+// .catch(error => console.error(error))
+// out = data;
+// console.log(data);
