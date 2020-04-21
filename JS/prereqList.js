5d191dee60523a9d6d447af79af71479a06ffaa1
plut092 <johan.m.engstrom92@gmail.com>
2020-04-21 12:19:13 +0200

    Hyperlinks with courseURL and courseName added.

--- JS/prereqList.js
+++ JS/prereqList.js
@@ -1,29 +1,34 @@
+// Code for testing
 /*
-function constructor(courseCode){
-        this.courseCode = courseCode;
-        this.prerequisites = [];
-        //this.courseName = name; //temp
-        //this.courseColor = color; //temp
-        this.courseURL = "https://www.kth.se/student/kurser/kurs/" + courseCode;
-    }
+function constructor(courseCode, courseName){
+    this.courseCode = courseCode;
+    this.prerequisites = [];
+    this.courseName = courseName; //temp
+    //this.courseColor = color; //temp
+    this.courseURL = "https://www.kth.se/student/kurser/kurs/" + courseCode;
+}
 */
 // Writes a nested list of the root parent and it's children
 function traverseGraph(node) {
     // Write outs the parent 
     // II1305: https://www.kth.se/student/kurser/kurs/courseCode
-    document.writeln(
-        "<li> " 
-            + node.courseCode
-            /*+ " - " + node.courseName */ //Remove comment to att coursename in format
-            + ": " + node.courseURL + 
-        "</li>"); 
-        document.write("<ul>") // creates nested list of children
+    document.writeln("<li><a href=" + node.courseURL + ">" + node.courseCode + "</a>"+ " - " + node.courseName +"</li>"); 
+        document.write("<ul>"); // creates nested list of children
             node.prerequisites.forEach(element => { //adding children to list
                 traverseGraph(element);
             });
-        document.write("</ul>")
+        document.write("</ul>");
 }
-
+// Code for testing
+/*
+node5 = new constructor("II1305", "Projekt inom informations- och kommunikationsteknik 7,5 hp");
+node4 = new constructor("II1305", "Projekt inom informations- och kommunikationsteknik 7,5 hp");
+node2 = new constructor("II1305", "Projekt inom informations- och kommunikationsteknik 7,5 hp");
+node3 = new constructor("II1305", "Projekt inom informations- och kommunikationsteknik 7,5 hp");
+node1 = new constructor("II1305", "Projekt inom informations- och kommunikationsteknik 7,5 hp");
+node1.prerequisites = [node2, node3];
+node2.prerequisites = [node4, node5];
+*/
 document.write("<ul>");
 traverseGraph(node1);
 document.write("</ul>");
