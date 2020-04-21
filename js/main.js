fb8e603b8a0c7c17427369c8c29447c491ee1b2e
Jingli512 <57395857+Jingli512@users.noreply.github.com>
2020-04-21 10:59:50 +0200

    final version of task 1

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 function searching(){
     var x = document.getElementById("coursecode").value;
-    let jsondata;
-    fetch('https://api.kth.se/api/kopps/v2/course/'+ x +'/detailedinformation').then(
-        function(u){return u.json();}
-    ).then(function(json){
-        jsondata = json;
-    })
+    fetch('https://api.kth.se/api/kopps/v2/course/'+ x +'/detailedinformation')
+    .then(res => res.json())
+    .then(json => console.log(json));
+
+
+    //MORE CODE
 }
