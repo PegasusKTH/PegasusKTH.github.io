1b393f2d37964f53747a84cf6ddaa8b84b11d74c
PatrikZhong <zhong.patrik@gmail.com>
2020-04-21 10:49:48 +0200

    test2

--- js/main.js
+++ js/main.js
@@ -1,18 +1,13 @@
-<<<<<<< HEAD
 let jsondata; //This is where the JSON object lies
-fetch('https://api.kth.se/api/kopps/v2/course/DD1321/detailedinformation').then(
+fetch('https://api.kth.se/api/kopps/v2/course/DD1321/detailedinformation    ').then(
     function(u){return u.json();}
 ).then(function(json){
     jsondata = json;
 
-=======
-function searching(){
-    var x = document.getElementById("coursecode").value;
-    let jsondata;
-    fetch('https://api.kth.se/api/kopps/v2/course/'+ x +'/detailedinformation').then(
-        function(u){return u.json();}
-    ).then(function(json){
-        jsondata = json;
-    })
->>>>>>> 8a7d2789f1b3336363acf67122c2cd80a3610480
 }
+)
+
+
+
+
+
