849df42b04f69691176eef9ea36362e183aff1e3
PatrikZhong <zhong.patrik@gmail.com>
2020-04-21 10:41:25 +0200

    pulling Jings code html and jscript

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-let jsondata;
+let jsondata; //This is where the JSON object lies
 fetch('https://api.kth.se/api/kopps/v2/course/DD1321/detailedinformation').then(
     function(u){return u.json();}
 ).then(function(json){
