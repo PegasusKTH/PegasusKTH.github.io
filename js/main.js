4daee782aca42bc0a3007e41768c6beb03023463
PatrikZhong <zhong.patrik@gmail.com>
2020-04-21 08:48:19 +0200

    workable code

--- js/main.js
+++ js/main.js
@@ -1,23 +1,43 @@
 
+// var store;
 
-var string = ' ';
-const request = async() => {
-    const json = await fetch('https://api.kth.se/api/kopps/v2/course/DD1321/detailedinformation').then(response => response.json());
-    console.log(json)
+// fetch('https://api.kth.se/api/kopps/v2/course/DD1321/detailedinformation')
+// .then(res => res.json())
+// .then(data => obj = data)
+// .then(() => console.log(obj))
+
+
+
+// async function getData(url){
+//     const response = await fetch('https://api.kth.se/api/kopps/v2/course/DD1321/detailedinformation');
+//     return response.json()
+// }
+
+// async function main() {
+//     const data = await getData('https://api.kth.se/api/kopps/v2/course/DD1321/detailedinformation');
+//     console.log(data)
+// }
+
+
+
+let jsondata;
+fetch('https://api.kth.se/api/kopps/v2/course/DD1321/detailedinformation').then(
+    function(u){return u.json();}
+).then(function(json){
+    jsondata = json;
 
 }
+)
 
-string = JSON.stringify(request());
+// function getData('https://api.kth.se/api/kopps/v2/course/DD1321/detailedinformation') {
+//     return fetch('https://api.kth.se/api/kopps/v2/course/DD1321/detailedinformation')
+//     .then(response => response.json())
+//     .then(result => result);
+// }
+
+// getData(url)
+// .then(result => console.log(result));
 
 
 
-// var out = '';
-// console.log(out);
-// fetch('https://api.kth.se/api/kopps/v2/course/DD1321/detailedinformation')
-// .then(response => response.json())
-// .then(data => {
-// console.log(data);
-// })
-// .catch(error => console.error(error))
-// out = data;
-// console.log(data);
+
