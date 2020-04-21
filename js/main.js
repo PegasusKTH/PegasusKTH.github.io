53a899c9d018fe2c16d3072193b9e17e207e5603
Flaxen <alexander.carlsson@live.se>
2020-04-21 09:23:46 +0200

    working implementation of tree construction

--- js/main.js
+++ js/main.js
@@ -1,22 +1,57 @@
 
-document.write("hello world");
-
+document.write("hello world123");
 
 class Node {
-    constructor(prerequisite, name, color){
-        this.prerequisite = prerequisite;
-        this.courseName = name;
-        this.courseColor = color;
-        this.children = [];
+    constructor(courseCode){
+        this.courseCode = courseCode;
+        //courseName = name; //temp
+        //courseColor = color; //temp
+        this.prerequisites = [];
+    }
+
+    jsonToArray(){
+      var courseCode = this.courseCode;
+        //patrikjing
+        //erikceline
+        //temp code returns three strings depending on called number
+
+        if (courseCode == "II1305") {
+          return ["ID1018", "ID1020", "IS1200"];
+        } else if (courseCode == "ID1018") {
+          return []
+        } else if (courseCode == "ID1020") {
+          return ["ID1018", "IS1200"]
+        } else if (courseCode == "IS1200") {
+          return []
+        }
+
     }
 
     addChild(node){
-        this.children.push(node);
+        this.prerequisites.push(node);
+    }
+
+    buildTree(){
+
+      var reqArr = this.jsonToArray();
+
+      for (var i = 0; i < reqArr.length; i++){
+        var temp = new Node(reqArr[i]);
+
+        this.addChild(temp);
+        temp.buildTree();
+      }
     }
 }
 
 
 
-temp1 = new Node("ID1020", "Algodata", "Svart");
-temp2 = new Node("IK1203", "Matte", "Vit");
-temp3 = new Node("SF1610", "Fysik", "Orange");
+
+test = function(firstCourseID){
+    for (var i = 0; i < 3; i++){
+        Node.buildTree(Node.jsonToArray(i));
+    }
+    //Node.buildTree(Node.jsonToArray(firstCourseID))
+}
+
+temp1 = new Node("II1305");
