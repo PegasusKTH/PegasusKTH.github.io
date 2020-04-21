b24dd81b95b61daf2efb8dbc334d1e0798686f51
Jingli512 <57395857+Jingli512@users.noreply.github.com>
2020-04-21 09:07:15 +0200

    changed files

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
-fetch('https://api.kth.se/api/kopps/v2/course/DD1321/detailedinformation')
+/*fetch('https://api.kth.se/api/kopps/v2/course/DD1321/detailedinformation')
 .then(res => res.json())
-.then(json => console.log(json));
+.then(json => console.log(json));*/
 
+let jsondata;
+fetch('https://api.kth.se/api/kopps/v2/course/DD1321/detailedinformation').then(
+    function(u){return u.json();}
+).then(function(json){
+    jsondata = json;
 
-/*let url = 'https://api.kth.se/api/kopps/v2/course/DD1321/detailedinformation';
-let response = await fetch(url);
-
-let commits = await response.json();
-
-console.log(commits);*/
+}
+)
