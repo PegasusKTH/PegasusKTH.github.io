ee9c9e5829e572fb794f8aad2f55aab293502426
Flaxen <alexander.carlsson@live.se>
2020-04-22 12:03:24 +0200

    integrated list and tree representation

--- js/prereqList.js
+++ js/prereqList.js
@@ -1,32 +1,11 @@
-// Is to be removed after graph constructor called instead
-function constructor(courseCode, courseName){
-    this.courseCode = courseCode;
-    this.prerequisites = [];
-    this.courseName = courseName; //temp
-    //this.courseColor = color; //temp
-    this.courseURL = "https://www.kth.se/student/kurser/kurs/" + courseCode;
-}
-// Is to be removed after graph constructor called instead
-function tempFunction() {
-    node5 = new constructor("II1305", "Kurs 5 - 15 hp");
-    node4 = new constructor("II1305", "Kurs 4 - 7,5 hp");
-    node3 = new constructor("II1305", "Kurs 3 - 7,5 hp");
-    node2 = new constructor("II1305", "Kurs 2 - 7,5 hp");
-    node1 = new constructor("II1305", "Projekt inom informations- och kommunikationsteknik 7,5 hp");
-    node1.prerequisites = [node2, node3];
-    node2.prerequisites = [node4, node5];
 
-    rootNode = node1;
-
-    return rootNode;
-}
 // types out list element - not collapsable
 function listElementNoChild(node) {
     document.writeln(
     "<li>"
         + "<a href=" + node.courseURL + ">" + node.courseCode + "</a>"
-        + " - " 
-        + node.courseName 
+        + " - "
+        + node.courseName
     +"</li>");
 }
 // types out list element - collapsable
@@ -34,15 +13,15 @@ function listElementWithChild(node) {
     document.writeln(
     '<li><span class="caret">'
         + "<a href=" + node.courseURL + ">" + node.courseCode + "</a>"
-        + " - " 
+        + " - "
         + node.courseName
         + "</span>");
-    
+
 }
 
 // Writes a nested list of the root parent and it's children
 function traverseGraph(node) {
-    // Write outs the parent 
+    // Write outs the parent
     // Displayed as: Hyperlink(CourseCode) - courseName
     if (node.prerequisites.length == 0) {
         listElementNoChild(node);
@@ -58,7 +37,7 @@ function traverseGraph(node) {
 }
 
 document.write('<ul id="prereqUL">');
-    traverseGraph(tempFunction()); // change tempFunction to the graph constructor (from Edvin & Alex)
+    traverseGraph(getRootNode());
 document.write("</ul>");
 
 var toggler = document.getElementsByClassName("caret");
