b82e02d0d801aaedc4f094bd388c8ce0d9f19669
Erabl <erablods@gmail.com>
2020-04-22 14:43:32 +0200

    yeet?

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@
 //(  ͡°  ͜ ʖ  ͡° ) w i d e  l e n n y  i s  h e r e  t o  t e s t  i f  c e l i n e  c a n  g i t
 //(° ͜ʖ°) she could
 function searching() {
-  var requiredCourse;
-  var recommendedCourse;
+  var requiredCourse = [];
+  var recommendedCourse = [];
   let jsondata;
   var recommendedCoursesArray = [];
   var requiredCoursesArray = [];
@@ -30,48 +30,51 @@ function searching() {
           var recommendedCourse = jsonOBJ.course.prerequisites;
       }
 
-//reqArrBoi = requiredCourse.match(/[A-Z][A-Z][0-9][0-9][0-9][0-9]/g);
-
-//for (var i = 0; i < reqArrBoi.length; i++) {
-//  console.log(reqArrBoi[i]);
-//}
-
       //finds all coursecodes in the required courses string
+          //do the splittysplit on req and rec courses
+          re
+
       var i;
-      //var reqArray = requiredCourse.match(/[A-Z][A-Z][0-9][0-9][0-9][0-9]/g);
-      for (i = 0; i < requiredCourse.match(/[A-Z][A-Z][0-9][0-9][0-9][0-9]/g).length; i++) {
-        requiredCoursesArray.push((requiredCourse.match(/[A-Z][A-Z][0-9][0-9][0-9][0-9]/g)[i]));
+      // var reqArray = new Array();
+      // var reqs  = requiredCourse.match(/[A-Z][A-Z][0-9][0-9][0-9][0-9]/g);
+      // console.log(reqs);
+      var reqArray = requiredCourse.match(/[A-Z][A-Z][0-9][0-9][0-9][0-9]/g); //reqs;
+      for (i = 0; i < reqArray.length; i++) {
+        requiredCoursesArray.push((reqArray[i]));
         //index zero since push pushes to first place in the array
-        document.write(requiredCoursesArray[0] + " ");
+          // document.write(requiredCoursesArray[0] + " ");
       }
 
       //finds all coursecodes in the recommended courses string
-      var i;
-      var recomArray = recommendedCourse.match(/[A-Z][A-Z][0-9][0-9][0-9][0-9]/g);
-      for (i = 0; i < recomArray.length; i++) {
-        recommendedCoursesArray.push(recomArray[i]);
+      // var recArr = new Array();
+      // var recs  = recommendedCourse.match(/[A-Z][A-Z][0-9][0-9][0-9][0-9]/g);
+      // console.log(recs);
+      var recArr = recommendedCourse.match(/[A-Z][A-Z][0-9][0-9][0-9][0-9]/g);//recs;
+      for (i = 0; i < recArr.length; i++) {
+        recommendedCoursesArray.push((recArr[i]));
         //index zero since push pushes to first place in the array
-        document.write(recomArray[i] + "this is doing something");
-        // console.log(recommendedCoursesArray[0] + " ");
+          // document.write(recommendedCoursesArray[0] + " ");
+          // console.log(recommendedCoursesArray[0] + " ");
       }
 
     /*pushing name of the course searched for, so that the index 0 of courses
     array always contains the name of the searched course, and the rest is two
     arrays with required and recommended courses*/
             var namn = new String(jsonOBJ.course.title);
-            // recommendedCoursesArray.push(namn);
-            var retArray = [namn , requiredCoursesArray, recommendedCoursesArray]
-            console.log(retArray);
-            document.write(retArray);
-            return retArray;
+                // recommendedCoursesArray.push(namn);
+            console.log([namn , requiredCoursesArray, recommendedCoursesArray])
+
+            return [namn , requiredCoursesArray, recommendedCoursesArray];
 
      }
 
 
 
     //).catch(error => console.error(error))
-    ).catch(error => document.write(" HI"))
-  }
+    ).catch(function(){
+      console.log("klar")
+      return([]);})
+      }
   catch(err) {
     return([]);
     // document.getElementById("demo").innerHTML = err.message;
