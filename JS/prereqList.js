6074492851aefccb8b5636f0e4128a906980e877
plut092 <johan.m.engstrom92@gmail.com>
2020-04-22 10:03:37 +0200

    Implemented tree view of list. Needs to start as showing and needs to omplement ID for every list element.

--- JS/prereqList.js
+++ JS/prereqList.js
@@ -21,19 +21,53 @@ function tempFunction() {
     return rootNode;
 }
 
+function listElementNoChild(node) {
+    document.writeln(
+    "<li>"
+        + "<a href=" + node.courseURL + ">" + node.courseCode + "</a>"
+        + " - " 
+        + node.courseName 
+    +"</li>");
+}
+
+function listElementWithChild(node) {
+    document.writeln(
+    '<li><span class="caret">'
+        + "<a href=" + node.courseURL + ">" + node.courseCode + "</a>"
+        + " - " 
+        + node.courseName
+        + "</span>");
+    
+}
+
 // Writes a nested list of the root parent and it's children
 function traverseGraph(node) {
     // Write outs the parent 
     // Displayed as: Hyperlink(CourseCode) - courseName
-    document.writeln("<li><a href=" + node.courseURL + ">" + node.courseCode + "</a>"+ " - " + node.courseName +"</li>"); 
-        document.write("<ul>"); // creates nested list of children
-            node.prerequisites.forEach(element => { //adding children to list
-                traverseGraph(element);
-            });
-        document.write("</ul>");
+    if (node.prerequisites.length == 0) {
+        listElementNoChild(node);
+    } else {
+            listElementWithChild(node);
+            document.write('<ul class="nested">'); // creates nested list of children
+                node.prerequisites.forEach(element => { //adding children to list
+                    traverseGraph(element);
+                });
+            document.write("</ul>");
+        document.write("</li>");
+    }
 }
 
-document.write("<ul>");
-traverseGraph(tempFunction()); // change tempFunction to the graph constructor (from Edvin & Alex)
+document.write('<ul id="prereqUL">');
+    traverseGraph(tempFunction()); // change tempFunction to the graph constructor (from Edvin & Alex)
 document.write("</ul>");
+
+var toggler = document.getElementsByClassName("caret");
+var i;
+
+for (i = 0; i < toggler.length; i++) {
+  toggler[i].addEventListener("click", function() {
+    this.parentElement.querySelector(".nested").classList.toggle("active");
+    this.classList.toggle("caret-down");
+  });
+}
 
