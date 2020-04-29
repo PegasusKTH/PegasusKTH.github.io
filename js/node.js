4a110b7701965e9dc13733188d2e781d83ef440f
plut092 <johan.m.engstrom92@gmail.com>
2020-04-29 10:09:26 +0200

    export tree on listIntegration is returning at the same format as moccode example. May still need ID's or some other work.

--- js/node.js
+++ js/node.js
@@ -1,145 +1,78 @@
-// globalIDcount = 0;
+
 var globalIDCounter = 0;
 
 class Node { // Originally Edvin/Alex
-    constructor(courseCode){
-      this.courseName;
-      this.courseCode = courseCode;
-      this.prerequisites = [];  // good for you
-      this.eligibility = [];    // REQUIRED
-      this.courseURL = "https://www.kth.se/student/kurser/kurs/" + courseCode;
-      this.parentNode = null;
-      this._json_id = null;
-    }
+  constructor(courseCode){
+    this.courseName;
+    this.courseCode = courseCode;
+    this.prerequisites = [];  // good for you
+    this.eligibility = [];    // REQUIRED
+    this.courseURL = "https://www.kth.se/student/kurser/kurs/" + courseCode;
+    this.parentNode = null;
+    this._json_id = null;
+  }
   
-    // Build tree from rootnode with prerequisite-list and eligibility-list
-    // Not implemented yet, takes first root node and recursively builds a tree
-    buildTree() {
-      //console.log(globalIDcount);
-      console.log(this._json_id);
+  // Build tree from rootnode with prerequisite-list and eligibility-list
+  // Not implemented yet, takes first root node and recursively builds a tree
+  buildTree() {
+    //console.log(globalIDcount);
+    console.log(this._json_id);
 
-      // replaces string children array with node child array for all index
-      for(var i = 0; i < this.eligibility.length; i++){
-        this.eligibility[i] = nodifyLookup(this.eligibility[i]);
-      }
-
-      for(var i = 0; i < this.eligibility.length; i++){
-        this.eligibility[i].buildTree();
-        this.eligibility[i].parentNode = this;
-      }
-
-      return this;
+    // replaces string children array with node child array for all index
+    for(var i = 0; i < this.eligibility.length; i++){
+      this.eligibility[i] = nodifyLookup(this.eligibility[i]);
     }
 
-
-
-    // formats singular node to array format for Treant.js
-    // format according to chart simple_chart_config, see treantTree.js and treant docs
-    formatNode() {
-        // root
-        if (this.parentNode == null) {
-          var nodeStructure = {
-            text: { name: this.courseCode }
-          };
-  
-          return nodeStructure;
-
-        } else {
-          
-        }
+    for(var i = 0; i < this.eligibility.length; i++){
+      this.eligibility[i].buildTree();
+      this.eligibility[i].parentNode = this;
+      this.eligibility[i].treeLevel = this.treeLevel + 1;
     }
 
-    exportTree(){
-      var nodeStructure = this.formatNode();
+    return this;
+  }
+
+
+
+  // formats singular node to array format for Treant.js
+  // format according to chart simple_chart_config, see treantTree.js and treant docs
+  formatNode() {
+    if (this.parentNode == null) {
+      var nodeStructure = {
+        text: { name: this.courseCode }
+      };
 
       return nodeStructure;
-    }
-
-    testStructure() {
-      var chart_config = {
-        chart: {
-            container: "#collapsable-example",
-
-            animateOnInit: true,
-            
-            node: {
-                collapsable: true
-            },
-            animation: {
-                nodeAnimation: "easeOutBounce",
-                nodeSpeed: 700,
-                connectorsAnimation: "bounce",
-                connectorsSpeed: 700
-            }
-        },
-        nodeStructure: {
-            image: "img/malory.png",
-            children: [
-                {
-                    image: "img/lana.png",
-                    collapsed: true,
-                    children: [
-                        {
-                            image: "img/figgs.png"
-                        }
-                    ]
-                },
-                {
-                    image: "img/sterling.png",
-                    childrenDropLevel: 1,
-                    children: [
-                        {
-                            image: "img/woodhouse.png"
-                        }
-                    ]
-                },
-                {
-                    pseudo: true,
-                    children: [
-                        {
-                            image: "img/cheryl.png"
-                        },
-                        {
-                            image: "img/pam.png"
-                        }
-                    ]
-                }
-            ]
-        }
+    } else {
+      var nodeStructure = {
+        text: { courseID: this.courseCode },
+        parent: { parent: this.parentNode.courseCode },
+      }
     };
-    return chart_config;
+
+    return nodeStructure;
+  }
+
+  exportTree() {
+    var nodeStructure = [this.formatNode()];
+
+    for (let i = 0; i < this.eligibility.length; i++) {
+      nodeStructure = nodeStructure.concat(this.eligibility[i].exportTree());      
     }
+    return nodeStructure;
   }
 
-  // function assignIDs(tree){
-  //   for(var i = 0; i < tree.eligibility.length; i++){
-  //     //tree._json_id = globalIDCounter + 1;
-  //     tree.counter = globalIDCounter++;
-  //     console.log("TIMES RUN LOOP: " + i + ", TIMES GLOBALED: "+ globalIDCounter);
-  //     assignIDs(tree.eligibility[i]);
-  //   }
+}
 
-  //   return tree;
-  // }
+// Takes the array with course information and 
+// creates a Node to be root node in the graph
+function nodifyLookup(courseCode){
+  var temp = lookup(courseCode); //here we have a JSON obj
+  var rootNode = new Node(courseCode);
+  
+  rootNode.courseName = temp[0];
+  rootNode.eligibility = temp[1];
+  rootNode.prerequisites = temp[2];
 
-  // Takes the array with course information and 
-  // creates a Node to be root node in the graph
-  function nodifyLookup(courseCode){
-    var temp = lookup(courseCode); //here we have a JSON obj
-    var rootNode = new Node(courseCode);
-    
-    rootNode.courseName = temp[0];
-    rootNode.eligibility = temp[1];
-    rootNode.prerequisites = temp[2];
-  
-    return rootNode;
-  }
-  
-  /*
-  lookup("1305") --> searching(lookup("1305")) 
-  @param: json @output: return example: ["name", ["course eligibilities"], ["course prerequisites"]]
-  
-  
-  */
-
-//  temp1 = new Node("II1305");
+  return rootNode;
+}
--- js/treantTree.js
+++ js/treantTree.js
@@ -12,89 +12,17 @@ var config = {
 
 };
 
-// temp1 temporary node. make this into function call
+// listIntegration has taken jsonObject and converted it into a tree of nodes
 var listIntegration = nodifyLookup("II1305").buildTree();
-//var wegotIDs = assignIDs(listIntegration);
-var testStructure = listIntegration.testStructure();
+
+// sets up nodes in the right format for the Treant
 var nodeStructure = listIntegration.exportTree();
-
-//var rootNode = nodifyLookup("II1305").buildTree();
-// var arr = rootNode.exportTree();
-
-// var listIntegration = temp1.buildTree();
-// var arr = temp1.exportTree();
 
 // combine config with tree for treant graph generation
 var simple_chart_config = [config].concat(nodeStructure);
-var simple_chart_config_test = testStructure;
 
+// PrereqList gets datastructure form here.
 function getRootNode() {
 	// console.log("return");
 	return listIntegration;
 }
-
-/*
-simple_chart_config = 
-{
-	chart:
-	{
-        container: "#tree-simple"
-    },
-    
-	nodeStructure: 
-	{
-		text: { name: "Parent node" 
-	},
-		
-		children: 
-		[
-            {
-				text: { name: "First child" }
-            },
-            {
-                text: { name: "Second child" }
-            }
-        ]
-    }
-};
-
-HOW CONCAT WORKS(?)
-1.
-config = {
-	container: "#chart",
-
-	node: {
-		collapsable: true
-	},
-	connectors: {
-		type: 'step'
-	}
-
-}
-
-2. CONCAT (NODESTRUCTURE)
-
-nodeStructure: {
-	text: { name: "Parent node" },
-	children: [
-		{
-			text: { name: "First child" }
-		},
-		{
-			text: { name: "Second child" }
-		}
-	]
-}
-
-3. CONCAT (CHILDREN)
-
-children: [
-		{
-			text: { name: "First child" }
-		},
-		{
-			text: { name: "Second child" }
-		}
-	]
-
-*/
