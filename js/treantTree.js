efb3b3af7b89a91f4d3aa14ca02784c083f5868a
plut092 <johan.m.engstrom92@gmail.com>
2020-04-30 14:13:16 +0200

    Funcionality of taking the JsonObject then creating a visual tree from it is complete

--- js/treantTree.js
+++ js/treantTree.js
@@ -1,42 +1,46 @@
+// TODO: SearchBox intergration needed on line 18. @param courseCode for nodifyLookupMAIN
+
+/*
+	This file takes a Course Code from index.html searching functionality and builds a tree
+
+	1.	NodifyLookupMAIN takes a course code and returns all information in a jsonObject
+	2.	A tree is recursivly built with buildTree and saved in listintegration where 
+		each node has information about:
+			courseCode
+			prerequisites
+			courseURL = "https://www.kth.se/student/kurser/kurs/" + courseCode;
+			parentNode
+			_json_id
+	3.	The tree in listIntegration is traversed row by row with BFS and gets assigned
+		and unique ID for each node in the tree. That is needed for the Treant library.
+	4.	nodestructure is a jsonObject useb by Treant and is created and formated 
+		by exportTree.
+	5.	Treant uses simple_chart_config that is the "config" concatenated with the 
+		"nodestructure" and visually shows the tree. 
+
+
+*/
 // config for tree container div
 // see html
-const treetop = "ID1020";
+var config = {
+	container: "#chart",
 
-let TREANT = {
-	chart: {
-    container: "#chart",
-		node: {collapsable: true},
-		connectors: {type: 'step', style: {opacity:100,}},
-		nodeAlign: "top",
-  },
-  nodeStructure: {
-    text: {
-      id: treetop,
-      name: undefined,
-      credits: undefined,
-    },
-    children: [],
-  }
-}
+	node: {
+		collapsable: true
+	},
+	connectors: {
+		type: 'step'
+	}
 
-let lookups = [TREANT.nodeStructure] //list of ids to look up. also using the TREANT structure instead of making a new one and then transplanting.
+};
 
-while(lookups.length > 0) { //we iterate until there are nothing left to look up
-  let active = lookups[0]; //defining the current object
-  lookups.shift();//removing the current object from future lookups
+// listIntegration has taken jsonObject and converted it into a tree of nodes
+var listIntegration = nodifyLookupMAIN("dd2420").buildTree(); // SearchBox integration needed.
 
+listIntegration.assignIdentifiers([]);
 
-  let result = lookup(active.text.id);
+// sets up nodes in the right format for the Treant
+var nodeStructure = listIntegration.exportTree();
 
-  let children = result[1];
-  active.text.name = ""+result[0]; // gotta coerce the "string" to act like a proper string
-  active.text.credits = 7.5; //result[3]+" hp";
-
-  for(i=0; i<children.length;i++) {
-    //babymakin'. we figure out the undefined parts when we lookup that specific course.
-    active.children[i] = {text:{id:children[i],name:undefined,credits:undefined,},parent:active,children:[]}
-    lookups.push(active.children[i]) //pushing the children to the list of IDs to look up
-  }
-}
-
-let TREE = new Treant(TREANT, $); //making the tre yo
+// combine config with tree for treant graph generation
+var simple_chart_config = [config].concat(nodeStructure);
