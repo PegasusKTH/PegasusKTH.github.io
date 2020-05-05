0e36ceb3c1f5b7943e554d1b2be57caad7c5f3e8
plut092 <johan.m.engstrom92@gmail.com>
2020-05-05 14:40:03 +0200

    integration continued

--- js/node.js
+++ js/node.js
@@ -110,6 +110,7 @@ class Node {
     // fully constructs tree object for later export
     buildTree() {
 
+      console.log(this.courseCode);
       var lookup = this.jsonToArray();
       var reqArr = lookup[1];
       this.setName(lookup[0]);
@@ -122,6 +123,7 @@ class Node {
           var temp = new Node(reqArr[i][0]);
 
           temp.equivalent = reqArr[i].shift();
+          this.addChild(temp);
 
         } else {
           var temp = new Node(reqArr[i]);
