f74e2a2a1efcd9df731da66295d508d660e002dc
Jingli512 <57395857+Jingli512@users.noreply.github.com>
2020-05-05 19:17:03 +0200

    Id and Name searching feature available

--- js/lookup.js
+++ js/lookup.js
@@ -71,10 +71,21 @@ function lookup(courseIDorName){ // Originally Patrick/Jing group
         courseArr[i] = temp.courseCode;
       }
     }
+    console.log(courseArr);
+    document.write("Here are the courses that are relevant to your searching: ");
   //If there're relavant courses found, generate one button for each course code
     if(courseArr.length>0){
      for(i = 0; i < courseArr.length; i++){
-        document.write('<a href="graph.html"><button type="button" onclick="firstall(courseArr[i])">'  + courseArr[i] + '</button></a>'); 
+      var path = "" + window.location.href;
+      //console.log(path);
+      url=[];
+      url = path.split("=");
+      url[1] = "=";
+      url[2] = courseArr[i];
+      finalUrl = url[0] + url[1] + url[2];
+      //console.log(finalURL[0]+finalURL[1]+finalURL[2]);
+      
+      document.write('<a href = \"' +finalUrl + '\" ><button type="button">'  + courseArr[i] + '</button></a>'); 
       }
     }
     //if there is no such course, go to the Course Not Found Page
